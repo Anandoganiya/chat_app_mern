a2402ab6a6f8dd4849f196ea816cfb19cb122044
Anand Oganiya <anandoganiya@gmail.com>
2023-08-26 23:44:00 +0530

    fix bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
-import Chatpage from "./pages/Chatpage";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path="/" element={<Homepage />} />
-        <Route path="/chats" element={<Chatpage />} />
+        <Route path="/chats" element={<ChatPage />} />
       </Routes>
     </div>
   );
